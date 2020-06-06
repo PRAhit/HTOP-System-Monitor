73e90a6be77df42713e1eebf52244f76c0c4088c
PRAhit <36682588+PRAhit@users.noreply.github.com>
2020-06-07 01:13:41 +0200

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -19,9 +19,9 @@ Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() {
-  vector<int> pids = LinuxParser::Pids();
+  vector<int> pids_ = LinuxParser::Pids();
   processes_.clear();
-  for (int i : pids) {
+  for (int i : pids_) {
     Process process(i);
     processes_.emplace_back(process);
   }
