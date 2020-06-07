af0746f9c693e37ca643a03655b889b1c2445e5e
PRAhit <36682588+PRAhit@users.noreply.github.com>
2020-06-07 09:03:18 +0200

    Update process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -17,14 +17,28 @@ Process::Process(int pid) { pid_ = pid; }
 int Process::Pid() { return pid_; }
 
 float Process::CpuUtilization() {
-  Total = LinuxParser::ActiveJiffies() + LinuxParser::IdleJiffies();
+ /* Total = LinuxParser::ActiveJiffies() + LinuxParser::IdleJiffies();
   Idle = LinuxParser::IdleJiffies();
   totalDiff = (float)(Total - PrevTotal);
   idleDiff = (float)(Idle-PrevIdle);
   CPU_percent = ((totalDiff - idleDiff)/totalDiff);
   PrevTotal = Total; 
   PrevIdle = Idle;
-  return CPU_percent; }
+  return CPU_percent; */
+
+string key , line, value ;
+    vector<string> data ;
+    int Hertz = sysconf(_SC_CLK_TCK);
+    std::ifstream filestream(LinuxParser::kProcDirectory+to_string(pid_)+LinuxParser::kStatFilename);
+    if(filestream.is_open()){
+        std::getline(filestream,line);
+        std::istringstream linestream(line) ; 
+        for (int i = 0; i < 22 ; i++)
+        {
+            linestream >> value ; 
+            data.push_back(value);
+        }
+}
 
 string Process::Command() { return LinuxParser::Command(Pid()); }
 
